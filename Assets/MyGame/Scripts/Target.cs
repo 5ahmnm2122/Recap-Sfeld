050c6c94f5f89c1e639d98f5037eac3d50e9f4f7
feldbacherSimone <simone.feldbacher03@htl-salzburg.ac.at>
2021-12-07 08:19:45 +0100

    add Target despawn

--- Assets/MyGame/Scripts/Target.cs
+++ Assets/MyGame/Scripts/Target.cs
@@ -1,5 +1,5 @@
 using UnityEngine;
-
+using System.Collections;
 
 public class Target : MonoBehaviour
 {
@@ -8,11 +8,17 @@ public class Target : MonoBehaviour
     private void Start()
     {
         gameManager = GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameManager>();
+        StartCoroutine(WaitForDespawn(4));
     }
     private void OnMouseDown()
     {
         Debug.Log("clicked");
         Destroy(gameObject);
         gameManager.AddScore(1); 
+    }
+
+    IEnumerator WaitForDespawn(float time) {
+        yield return new WaitForSeconds(time);
+        Destroy(gameObject);
     }
 }
